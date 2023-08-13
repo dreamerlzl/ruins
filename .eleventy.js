76c5bb4fbc7f831244d484ce50abdc0230025703
dreamerlzl <875350879@qq.com>
2023-08-13 23:26:49 +0800

    feat: add jp trip

--- .eleventy.js
+++ .eleventy.js
@@ -1,80 +1,84 @@
-const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight')
-const markdownIt = require('markdown-it')
-const markdownItAnchor = require('markdown-it-anchor')
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const markdownIt = require("markdown-it");
+const markdownItAnchor = require("markdown-it-anchor");
 const searchFilter = require("./src/js/searchFilter.js");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
 const { DateTime } = require("luxon");
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   // for elasticlunr search
-  eleventyConfig.addFilter("search", searchFilter)
+  eleventyConfig.addFilter("search", searchFilter);
   eleventyConfig.addPassthroughCopy("src/js");
-  eleventyConfig.addCollection("posts", collection => {
-    return [...collection.getFilteredByGlob("./src/posts/**/*.md")]
-  })
-
-  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
-    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
+  eleventyConfig.addCollection("posts", (collection) => {
+    return [...collection.getFilteredByGlob("./src/posts/**/*.md")];
   });
 
-  eleventyConfig.addFilter("readableDate", dateObj => {
-    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("dd LLL yyyy");
+  eleventyConfig.addFilter("htmlDateString", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
+  });
+
+  eleventyConfig.addFilter("readableDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
+      "dd LLL yyyy",
+    );
   });
 
   // Plugins
-  eleventyConfig.addPlugin(syntaxHighlight)
-  
+  eleventyConfig.addPlugin(syntaxHighlight);
+  eleventyConfig.addPlugin(pluginRss);
+
   // To enable merging of tags
-  eleventyConfig.setDataDeepMerge(true)
+  eleventyConfig.setDataDeepMerge(true);
 
   // Copy these static files to _site folder
-  eleventyConfig.addPassthroughCopy('src/assets')
-  eleventyConfig.addPassthroughCopy('src/manifest.json')
+  eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPassthroughCopy("src/manifest.json");
 
   // To create excerpts
   eleventyConfig.setFrontMatterParsingOptions({
     excerpt: true,
-    excerpt_alias: 'post_excerpt',
-    excerpt_separator: '<!-- excerpt -->'
-  })
+    excerpt_alias: "post_excerpt",
+    excerpt_separator: "<!-- excerpt -->",
+  });
 
   // To create a filter to determine duration of post
-  eleventyConfig.addFilter('readTime', (value) => {
-    const content = value
-    const textOnly = content.replace(/(<([^>]+)>)/gi, '')
-    const readingSpeedPerMin = 450
-    return Math.max(1, Math.floor(textOnly.length / readingSpeedPerMin))
-  })
+  eleventyConfig.addFilter("readTime", (value) => {
+    const content = value;
+    const textOnly = content.replace(/(<([^>]+)>)/gi, "");
+    const readingSpeedPerMin = 450;
+    return Math.max(1, Math.floor(textOnly.length / readingSpeedPerMin));
+  });
 
   // Enable us to iterate over all the tags, excluding posts and all
-  eleventyConfig.addCollection('tagList', collection => {
-    const tagsSet = new Set()
-    collection.getAll().forEach(item => {
-      if (!item.data.tags) return
+  eleventyConfig.addCollection("tagList", (collection) => {
+    const tagsSet = new Set();
+    collection.getAll().forEach((item) => {
+      if (!item.data.tags) return;
       item.data.tags
-        .filter(tag => !['posts', 'all'].includes(tag))
-        .forEach(tag => tagsSet.add(tag))
-    })
-    return Array.from(tagsSet).sort()
-  })
+        .filter((tag) => !["posts", "all"].includes(tag))
+        .forEach((tag) => tagsSet.add(tag));
+    });
+    return Array.from(tagsSet).sort();
+  });
 
-  const md = markdownIt({ html: true, linkify: true })
-  md.use(markdownItAnchor, { 
-    level: [1, 2], 
-    permalink: markdownItAnchor.permalink.headerLink({ 
+  const md = markdownIt({ html: true, linkify: true });
+  md.use(markdownItAnchor, {
+    level: [1, 2],
+    permalink: markdownItAnchor.permalink.headerLink({
       safariReaderFix: true,
-      class: 'header-anchor',
-    })
-  })
-  eleventyConfig.setLibrary('md', md)
+      class: "header-anchor",
+    }),
+  });
+  eleventyConfig.setLibrary("md", md);
 
   // asset_img shortcode
-  eleventyConfig.addLiquidShortcode('asset_img', (filename, alt) => {
-    return `<img class="my-4" src="/assets/img/posts/${filename}" alt="${alt}" />`
-  })
+  eleventyConfig.addLiquidShortcode("asset_img", (filename, alt) => {
+    return `<img class="my-4" src="/assets/img/posts/${filename}" alt="${alt}" />`;
+  });
 
   return {
     dir: {
-      input: 'src'
-    }
-  }
-}
+      input: "src",
+    },
+  };
+};
